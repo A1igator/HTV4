4fe4b3ff51a7e2ca223ad32137f8ef017ca73d1a
Ali Abdoli <aliabdoli1970@gmail.com>
2020-02-15 19:06:03 -0500

    fixed map view

--- src/map/MapView.js
+++ src/map/MapView.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap, DirectionsRenderer } from 'react-google-maps';
+const { compose, withProps, lifecycle } = require("recompose");
+const {
+  withGoogleMap,
+  GoogleMap,
+  DirectionsRenderer,
+} = require("react-google-maps");
+
+const MapWithADirectionsRenderer = compose(
+  withGoogleMap,
+  lifecycle({
+    componentDidMount() {
+      const DirectionsService = new window.google.maps.DirectionsService();
+
+      DirectionsService.route({
+        origin: new window.google.maps.LatLng(41.8507300, -87.6512600),
+        destination: new window.google.maps.LatLng(41.8525800, -87.6514100),
+        travelMode: window.google.maps.TravelMode.DRIVING,
+      }, (result, status) => {
+        if (status === window.google.maps.DirectionsStatus.OK) {
+          this.setState({
+            directions: result,
+          });
+        } else {
+          console.error(`error fetching directions ${result}`);
+        }
+      });
+    }
+  })
+)(props =>
+  <GoogleMap
+    defaultZoom={7}
+    defaultCenter={new window.google.maps.LatLng(41.8507300, -87.6512600)}
+  >
+    {props.directions && <DirectionsRenderer directions={props.directions} />}
+  </GoogleMap>
+);
+
 class MapView extends Component {
    render() {
-   const GoogleMapExample = withGoogleMap(props => (
-      <GoogleMap
-        defaultCenter = { { lat: 43.652070, lng: -79.385015 } }
-        defaultZoom = { 13 }
-      >
 
-<DirectionsRenderer origin={{ lat: 43.651070, lng: -79.347015 }} destination={{ lat: 43.652070, lng: -79.385015 }} />
-      </GoogleMap>
-   ));
    return(
       <div>
-        <GoogleMapExample
+        <MapWithADirectionsRenderer
           containerElement={ <div style={{ height: `100vh`, width: '100vw' }} /> }
           mapElement={ <div style={{ height: `100%` }} /> }
         />
