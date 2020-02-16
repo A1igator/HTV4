b70d37afd03d57a8ff37d7bcfe19fda028a11a53
Nick "bass man" Adams <nicholas.adams@mail.utoronto.ca>
2020-02-16 00:17:23 -0500

    Made map and tables

--- src/map/MapView.js
+++ src/map/MapView.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
-const { compose, withProps, lifecycle } = require("recompose");
+import { Dropdown } from 'semantic-ui-react';
+import { Icon, Label, Menu, Table } from 'semantic-ui-react';
+import './MapView.css';
+const { compose, withProps, lifecycle, withStateHandlers } = require("recompose");
 const {
   withGoogleMap,
   GoogleMap,
   DirectionsRenderer,
+  Marker,
+  InfoWindow,
 } = require("react-google-maps");
+
+var test = [['ChIJiRLw_wdIK4gRmgkLJuA61Xg','40','Barber'],
+ ['ChIJFRRC17Q2K4gRrDLxhzRnQJ4','35','Zoo'],
+  ['ChIJ44AtJc00K4gRnPYkPlAQ648','70','Store'],
+   ['ChIJxc6GqN3M1IkR5KeSVS1QcOY','25','School'],
+    ['ChIJo6RbIvLZ1IkRfVHfYVKwK1M','5','Aquarium']]; 
 
 const MapWithADirectionsRenderer = compose(
   withGoogleMap,
   lifecycle({
     componentDidMount() {
       const DirectionsService = new window.google.maps.DirectionsService();
-
+      var wypnts = [];
+      for (var i = 0; i < test.length; i++)
+      {
+        wypnts.push({
+          location: {'placeId': test[i][0]}
+        });
+      }
+      var my_origin = wypnts.shift();
+      var my_destination = wypnts.pop();
+      console.log(my_origin);
+      console.log(my_origin.location.placeId);
       DirectionsService.route({
-        origin: new window.google.maps.LatLng(41.8507300, -87.6512600),
-        destination: new window.google.maps.LatLng(41.8525800, -87.6514100),
-        travelMode: window.google.maps.TravelMode.DRIVING,
+        origin: {
+          'placeId': my_origin.location.placeId
+        },
+        destination: {
+          'placeId': my_destination.location.placeId
+        },
+        waypoints: wypnts,
+        optimizeWaypoints: true,
+        travelMode: window.google.maps.TravelMode.DRIVING
       }, (result, status) => {
         if (status === window.google.maps.DirectionsStatus.OK) {
           this.setState({
@@ -35,7 +62,42 @@ const MapWithADirectionsRenderer = compose(
     {props.directions && <DirectionsRenderer directions={props.directions} />}
   </GoogleMap>
 );
-
+const modes = [
+  {
+    key: 'DRIVING',
+    text: 'Driving',
+    value: 'DRIVING',
+    //image: { avatar: true, src: 'https://imgur.com/QknR12N' },
+  },
+  {
+    key: 'WALKING',
+    text: 'Walking',
+    value: 'WALKING',
+    //image: { src: '/transport_icons/a.jpg' },
+  },
+  {
+    key: 'BIKING',
+    text: 'Biking',
+    value: 'BIKING',
+    //image: { avatar: true, src: '/images/avatar/small/stevie.jpg' },
+  },
+  {
+    key: 'TRANSIT',
+    text: 'Transit',
+    value: 'TRANSIT',
+   // image: { avatar: true, src: '/images/avatar/small/christian.jpg' },
+  }
+];
+var ctgries = [];
+for (var i = 0; i < test.length; i++)
+{
+  ctgries.push(test[i][2]);
+}
+var arv_times = [];
+for (var i = 0; i < test.length; i++)
+{
+  arv_times.push(test[i][1]);
+}
 class MapView extends Component {
    render() {
 
@@ -45,6 +107,87 @@ class MapView extends Component {
           containerElement={ <div style={{ height: `100vh`, width: '100vw' }} /> }
           mapElement={ <div style={{ height: `100%` }} /> }
         />
+        <div className="info-table">
+        <Table celled>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>Location</Table.HeaderCell>
+        <Table.HeaderCell>Category</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+      <Table.Row >
+        <Table.Cell>A</Table.Cell>
+        <Table.Cell>{ctgries[0]}</Table.Cell>
+      </Table.Row>
+      {test.length >= 2 && (
+        <>
+      <Table.Row >
+        <Table.Cell>B</Table.Cell>
+        <Table.Cell>{ctgries[1]}</Table.Cell>
+      </Table.Row>
+      </>)}
+      {test.length >= 3 && (
+        <>
+      <Table.Row >
+        <Table.Cell>C</Table.Cell>
+        <Table.Cell>{ctgries[2]}</Table.Cell>
+      </Table.Row>
+      </>)}
+      {test.length >= 4 && (
+        <>
+      <Table.Row >
+        <Table.Cell>D</Table.Cell>
+        <Table.Cell>{ctgries[3]}</Table.Cell>
+      </Table.Row>
+      </>)}
+      {test.length >= 5 && (
+        <>
+      <Table.Row >
+        <Table.Cell>E</Table.Cell>
+        <Table.Cell>{ctgries[4]}</Table.Cell>
+      </Table.Row>
+      </>)}
+      {test.length >= 6 && (
+        <>
+      <Table.Row >
+        <Table.Cell>F</Table.Cell>
+        <Table.Cell>{ctgries[5]}</Table.Cell>
+      </Table.Row>
+      </>)}
+      {test.length >= 7 && (
+        <>
+      <Table.Row >
+        <Table.Cell>G</Table.Cell>
+        <Table.Cell>{ctgries[6]}</Table.Cell>
+      </Table.Row>
+      </>)}
+      {test.length >= 8 && (
+        <>
+      <Table.Row >
+        <Table.Cell>H</Table.Cell>
+        <Table.Cell>{ctgries[7]}</Table.Cell>
+      </Table.Row>
+      </>)}
+      {test.length >= 9 && (
+        <>
+      <Table.Row >
+        <Table.Cell>I</Table.Cell>
+        <Table.Cell>{ctgries[8]}</Table.Cell>
+      </Table.Row>
+      </>)}
+    </Table.Body>
+  </Table>
+        </div>
+        <div className="floating-panel" id="travel-mode">
+        <b>Mode of Travel:</b>
+        <Dropdown
+          placeholder='Driving'
+          fluid
+          selection
+          options={modes}
+        />
+        </div>
       </div>
    );
    }
